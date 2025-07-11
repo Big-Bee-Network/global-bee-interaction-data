2665f80ec43db7c6ef4c94fcddb50b2eaebe33fa
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-07-11 13:53:54 -0500

    shorten script names

--- concat-names.sh
+++ concat-names.sh
@@ -6,8 +6,8 @@
 cat ls-globi-review-ids.log\
  | head -1\
  | xargs -I{} bash -c 'curl "https://zenodo.org/records/{}/files/indexed-names-resolved.tsv.gz" | gunzip | head -1 | gzip'
- > indexed-names-resolved-all.tsv.gz
+ > concat-names.tsv.gz
 
 cat ls-globi-review-ids.log\
  | xargs -I{} bash -c 'curl "https://zenodo.org/records/{}/files/indexed-names-resolved.tsv.gz" | gunzip | tail -n+2 | gzip'\
- >> indexed-names-resolved-all.tsv.gz
+ >> concat-names.tsv.gz
